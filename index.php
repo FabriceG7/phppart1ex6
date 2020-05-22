9db91c27f69235685887966ed89eea501e077b8b
FabriceG7 <goret.goret@sfr.fr>
2020-05-22 15:41:54 +0200

    phppart1ex6

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$number = 140;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8/">
+    <title>php part1 ex6</title>
+  </head>
+  <body>
+    <h1>
+      <?php
+      echo ($number + 30)/2;
+      ?>
+    </h1>
+  </body>
+</html>
